029802822e11219a99e4a69785972c5813d00325
Ryan Ghaida <ryaboug@gmail.com>
2018-03-02 15:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,21 +18,22 @@ function takeANumber (DeliLine, name) {
 
 
 function currentLine(DeliLine) {
-  //To Iterate over array I created a new array.
 var newArray = [];
-//!0 Truey and thus will execute when array has legnth 0.
+
 if (!DeliLine.length) {
   return "The line is currently empty."
 }
 
-else for(var i = 0; i<DeliLine.length;i++) {
+for(var i = 0; i<DeliLine.length;i++) {
 
-newArray.push(`${i+1}. ${DeliLine[i]}`)
+newArray.push([i+1] + "." + DeliLine[i])
+ console.log(newArray)
 }
 
-return "The line is currently: " + newArray.join(", ")
-
+return "The line is currently:" + newArray.join()
 }
+
+
 
 function nowServing (DeliLine) {
 
